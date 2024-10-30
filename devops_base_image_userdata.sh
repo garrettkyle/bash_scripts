6f9ea6bdc39f93e54e32d6ed1f00e88b4f2722a1
Garrett Kyle <me@me.com>
2024-10-30 10:23:20 -0700

    added command to fix apparmor for docker permissions denied issue

--- devops_base_image_userdata.sh
+++ devops_base_image_userdata.sh
@@ -31,3 +31,4 @@ DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.30.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+aa-remove-unknown # fix for docker not letting you stop/kill containers

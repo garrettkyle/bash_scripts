dfa907174e6c186d360364c2419d2a66b96f898f
Garrett Kyle <me@me.com>
2024-10-30 09:15:10 -0700

    added docker compose commands

--- devops_base_image_userdata.sh
+++ devops_base_image_userdata.sh
@@ -27,3 +27,7 @@ sudo usermod -aG docker $USER
 sudo apt update
 sudo apt upgrade -y
 sudo mkdir /git
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.30.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
